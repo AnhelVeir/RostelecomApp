83d2bad3f54d75c0da28ea37aea2a1911a583256
Mary <mary.gaidomak@gmail.com>
2020-07-12 21:43:24 +0400

    Импорт библиотек

--- RostelecomApp/RostelecomApp.py
+++ RostelecomApp/RostelecomApp.py
@@ -1,1 +1,13 @@
-
+# Испорт библиотек
+# Kivy - для граф. интерфейса
+# Pandas - для работы с базами данных
+# Re - для работы с регулярными выражениями
+# Dfgui - для визуалтзации pandas dataframe (таблицы)
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.textinput import TextInput
+import pandas as pd
+import re
+import dfgui
